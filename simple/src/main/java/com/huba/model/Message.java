f5e34594f81f0f4519beb331b9722191b9eabbdf
albuhuba <albuhuba@yahoo.com>
2016-11-05 00:08:57 +0100

    Hibernate setup
    * additional flyway boxfuse for scalability and production purposes
    * tomcat and spring boot
    * external configurations via YML file
    * Log4j as logger
    * update script for Flyway

---
+++ simple/src/main/java/com/huba/model/Message.java
@@ -0,0 +1,43 @@
+package com.huba.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+/**
+ *
+ * @author Huba Albu {ha@viabill.com}
+ *
+ **/
+@Entity(name = "message")
+public class Message {
+
+	@Id
+	@Column(updatable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, updatable = false, length = 255)
+	private String message;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
